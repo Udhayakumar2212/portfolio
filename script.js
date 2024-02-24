8a74c2484de487fada4786c6cbecb89ffa5d92e1
UDHAYAKUMAR M <160875286+Udhayakumar2212@users.noreply.github.com>
2024-02-24 14:37:18 +0530

    Added New Files

---
+++ script.js
@@ -0,0 +1,42 @@
+const header = document.querySelector("header");
+
+window.addEventListener ("scroll", function() {
+    header.classList.toggle ("sticky", window.scrollY >120);
+});
+
+const form = document.querySelector("form");
+const fullName = document.getElementById("name");
+const email = document.getElementById("email");
+const mess = document.getElementById("message");
+
+function sendEmail() {
+    const bodyMessage = `Full Name: ${fullName.value}<br>Email: ${email.value}<br>Message: ${mess.value}`;
+
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "udhayam27051999@gmail.com",
+        Password : "B709B01DB5909F6896E436D86F592625646C",
+        To : 'udhayam27051999@gmail.com',
+        From : "udhayam27051999@gmail.com",
+        Subject : "Message from Client",
+        Body : bodyMessage
+    }).then(
+      message => {
+        if(message== "OK"){
+            Swal.fire({
+                title: "Success!",
+                text: "Message sent Successfully",
+                icon: "success"
+              });
+        }
+      }
+    );
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    sendEmail();
+    form.reset();
+    return false;
+});
